c075f97a992aeb508ec61f4dc16c063150eab86c
Yuwa Aoki <s20C1001GN@s.chibakoudai.jp>
2021-11-02 13:23:52 +0900

    update

--- NR.c
+++ NR.c
@@ -46,7 +46,19 @@ int main()
 		dy = get_dy1( x );
 		d = y / dy;
 		x += d;
-		printf("試行%d, x=%lf\n", n, x);
+		printf("x^2+4*x+4, 試行%d, x=%lf\n", n, x);
+	}while(n <= nM && fabs(d) > e );
+
+	x = 1;
+	n = 0;
+	do{
+		n++;
+		y = get_y2( x );
+		y *= -1;
+		dy = get_dy2( x );
+		d = y / dy;
+		x += d;
+		printf("x^2-4, 試行%d, x=%lf\n", n, x);
 	}while(n <= nM && fabs(d) > e );
 
 
